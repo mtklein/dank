81b68e9a5a3d753d9de5d95536632a580b76194c
Mike Klein <mtklein@gmail.com>
2025-10-31 09:31:42 -0500

    Implement dank via encoded instructions

--- dank.c
+++ dank.c
@@ -1,28 +1,22 @@
 #if defined(__x86_64__)
 
-#define CAT_INNER(a, b) a##b
-#define CAT(a, b) CAT_INNER(a, b)
-#define BUILD3(a, b, c) CAT(CAT(a, b), c)
-#define BUILD4(a, b, c, d) CAT(BUILD3(a, b, c), d)
-#define BUILD5(a, b, c, d, e) CAT(BUILD4(a, b, c, d), e)
-#define HEX5(a, b, c, d, e) CAT(0x, BUILD5(a, b, c, d, e))
-
-#define DANK_CONST HEX5(4, 2, 0, 6, 9)
+#include <stdint.h>
 
 int dank(int x) {
-    return x + DANK_CONST;
+    uint32_t result;
+    __asm__ __volatile__(
+        ".byte 0x89, 0xf8\n"
+        ".byte 0x05, 0x69, 0x20, 0x04, 0x00\n"
+        : "=a"(result)
+        : "D"(x)
+        : "cc");
+    return (int)result;
 }
 
-#undef DANK_CONST
-#undef HEX5
-#undef BUILD5
-#undef BUILD4
-#undef BUILD3
-#undef CAT
-#undef CAT_INNER
-
 #else
+
 int dank(int x) {
     return x;
 }
+
 #endif
